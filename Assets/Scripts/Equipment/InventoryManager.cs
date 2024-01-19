19fda724c9bc3cb06b1620a5d685cd70917364ca
Helcostr <15018549+Helcostr@users.noreply.github.com>
2024-01-19 11:18:42 -0500

    make variables avaiable to be obtained (constantly constructing new collector)

--- Assets/Scripts/Equipment/InventoryManager.cs
+++ Assets/Scripts/Equipment/InventoryManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Collections;
 using UnityEngine;
 
@@ -37,4 +38,19 @@ public class InventoryManager : MonoBehaviour {
         else if (equip is Legs)
             legsSlot = equip as Legs;
     }
+
+    private List<Equipment> getAllEquipped() {
+        // Hm... my laziness is going to be a doomed scenario...
+        return new List<Equipment> { helmetSlot, arm1Slot, arm2Slot, chestSlot, legsSlot };
+    }
+    public float getHealthMultiply() { return getAllEquipped().Aggregate(1f, (acc, cur) => acc *= cur.getHealthMultiply()); }
+    public int getHealthAdd() { return getAllEquipped().Aggregate(0, (acc, cur) => acc += cur.getHealthAdd()); }
+    public float getAttackSpeedMultiply() { return getAllEquipped().Aggregate(1f, (acc, cur) => acc *= cur.getAttackSpeedMultiply()); }
+    public int getAttackSpeedAdd() { return getAllEquipped().Aggregate(0, (acc, cur) => acc += cur.getAttackSpeedAdd()); }
+    public float getAttackDamageMultiply() { return getAllEquipped().Aggregate(1f, (acc, cur) => acc *= cur.getAttackDamageMultiply()); }
+    public int getAttackDamageAdd() { return getAllEquipped().Aggregate(0, (acc, cur) => acc += cur.getAttackDamageAdd()); }
+    public float getMoveSpeedMultiply() { return getAllEquipped().Aggregate(1f, (acc, cur) => acc *= cur.getMoveSpeedMultiply()); }
+    public int getMoveSpeedAdd() { return getAllEquipped().Aggregate(0, (acc, cur) => acc += cur.getMoveSpeedAdd()); }
+    public float getDamageReductionMultiply() { return getAllEquipped().Aggregate(1f, (acc, cur) => acc *= cur.getDamageReductionMultiply()); }
+    public int getDamageReductionAdd() { return getAllEquipped().Aggregate(0, (acc, cur) => acc += cur.getDamageReductionAdd()); }
 }

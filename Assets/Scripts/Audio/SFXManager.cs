dd20b368f85265d40d53f83efbf03f8bc6ff94c6
cothon01 <christos.othonos.stratos@gmail.com>
2024-01-20 14:21:51 -0500

    Game Audio Management 3
    +Added function in SFXManager to find unoccupied AudioSource component.
    + Added function that plays sound on AudioSource component
    + Added public functions for playing 2d and 3d sounds
    + Overloads for playing specific AudioClips, or SFXClip (for random clip)

--- Assets/Scripts/Audio/SFXManager.cs
+++ Assets/Scripts/Audio/SFXManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 /// <summary>
 /// A useful utility for playing audio that solves the issues of having audiosources in your gameobjects:
@@ -12,6 +13,11 @@ using UnityEngine;
 /// </summary>
 public class SFXManager : Singleton<SFXManager>
 {
+
+    [Tooltip("The default sound channel")]
+    public AudioMixerGroup mixerGroup;
+
+
 
     [Header("Parameters")]
     [SerializeField, Tooltip("How many audio sources to generate")]
@@ -40,5 +46,91 @@ public class SFXManager : Singleton<SFXManager>
         }
     }
 
-    // TODO: Implement functions
+    /// <summary>
+    /// Helper function that finds the next available audio source
+    /// </summary>
+    /// <returns></returns>
+    protected AudioSource FindNextSource()
+    {
+        AudioSource source = sources[0];
+        for (int i = 0; i < sources.Length; i++)
+        {
+            if (sources[i].isPlaying == false)
+            {
+                source = sources[i];
+                break;
+            }
+        }
+        return source;
+    }
+
+    /// <summary>
+    /// Helper function that plays a specific clip on an available audiosource.
+    /// </summary>
+    /// <param name="clip">AudioClip to play</param>
+    /// <param name="position">Where the sound is located</param>
+    /// <param name="volume">How loud the sound is</param>
+    /// <param name="pitch">The sound pitch</param>
+    /// <param name="spatialBlend">2d vs 3d sound</param>
+    protected void Play(AudioClip clip, Vector3 position, float volume, float pitch, float spatialBlend )
+    {
+        // Find first available audiosource
+        AudioSource source = FindNextSource();
+        source.transform.position = position;
+
+        // Fill in audiosource parameters
+        source.spatialBlend = spatialBlend;
+        source.volume = volume;
+        source.clip = clip;
+        source.pitch = pitch;
+        source.outputAudioMixerGroup = mixerGroup;
+
+        // Play sound
+        source.Play();
+    }
+
+    /// <summary>
+    /// Helper function for playing a random sound effect.
+    /// </summary>
+    /// <param name="sfxClip"></param>
+    /// <param name="position"></param>
+    /// <param name="volume"></param>
+    /// <param name="spatialBlend"></param>
+    protected void Play(SFXClip sfxClip, Vector3 position, float volume, float spatialBlend)
+    {
+        // Reuse core helper function
+        var clip = sfxClip.NextClip();
+        var pitch = sfxClip.NextPitch();
+        Play(clip, position, volume, pitch, spatialBlend);
+    }
+
+    /// <summary>
+    /// Publicly accessible function. Plays a sound clip.
+    /// </summary>
+    /// <param name="sfxClip"></param>
+    public static void Play(SFXClip sfxClip, float volume = 1.0f) => instance.Play(sfxClip, Vector3.zero, volume, 0.0f);
+
+    /// <summary>
+    /// Publicly accessible functions that plays 3d audio.
+    /// </summary>
+    /// <param name="sfxClip"></param>
+    /// <param name="position"></param>
+    public static void Play3D(SFXClip sfxClip, Vector3 position, float volume = 1.0f) => instance.Play(sfxClip, position, volume, 1.0f);
+
+
+    /// <summary>
+    /// Publicly accessible function. Plays a sound clip.
+    /// </summary>
+    /// <param name="sfxClip"></param>
+    public static void Play(AudioClip clip, float volume = 1.0f, float pitch = 1.0f) => instance.Play(clip, Vector3.zero, volume,pitch, 0.0f);
+
+    /// <summary>
+    /// Publicly accessible functions that plays 3d audio.
+    /// </summary>
+    /// <param name="sfxClip"></param>
+    /// <param name="position"></param>
+    public static void Play3D(AudioClip clip, Vector3 position, float volume = 1.0f, float pitch = 1.0f) => instance.Play(clip, position, volume,pitch, 1.0f);
+
+
+
 }

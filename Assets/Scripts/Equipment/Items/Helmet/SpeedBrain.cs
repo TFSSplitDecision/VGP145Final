7bc52dc2f6177a4656fa3ac3dd520f401ed9f50a
Seangao97 <Seangao97@gmail.com>
2024-01-21 17:12:16 -0500

    Sean changes (#42)

---
+++ Assets/Scripts/Equipment/Items/Helmet/SpeedBrain.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Data/SpeedBrain", order = 5)]
+public class SpeedBrain : Helmet
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

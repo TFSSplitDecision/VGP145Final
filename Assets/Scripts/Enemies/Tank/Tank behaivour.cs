ef2b9f8773717aff7661d6c75846f8c6db0f346b
Jeroldine <jeromeremigio@gmail.com>
2024-01-23 19:26:59 -0500

    Enemy Tank Behaviour
    - follow player
    - rotate to face player

--- Assets/Scripts/Enemies/Tank/Tank behaivour.cs
+++ Assets/Scripts/Enemies/Tank/Tank behaivour.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Tankbehaivour : MonoBehaviour
 {
+    [SerializeField] Transform target; 
+    [SerializeField] float speed;
+    [SerializeField] float turnSpeed;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetPos = new Vector3(target.position.x, 0, target.position.z);
+        Vector3 targetDir = (targetPos - new Vector3(transform.position.x, 0, transform.position.z)).normalized;
+
+        float singleStep = turnSpeed * Time.deltaTime;
+        Vector3 turnDir = Vector3.RotateTowards(transform.forward, targetDir, singleStep, 0.0f);
+        Debug.DrawRay(transform.position, 10*turnDir, Color.red);
+
+        //transform.Translate(speed * Time.deltaTime * targetDir);
+        rb.velocity = speed * targetDir;
+        transform.rotation = Quaternion.LookRotation(turnDir);
     }
 }
---
+++ Assets/Scripts/testPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testPlayer : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float fInput = Input.GetAxisRaw("Vertical");
+        Vector3 moveDir = new Vector3(hInput, 0, fInput).normalized;
+
+        transform.Translate(speed *Time.deltaTime* moveDir.x, 0, speed *Time.deltaTime * moveDir.z);
+    }
+}

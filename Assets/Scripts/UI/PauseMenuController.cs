ce90be9b0507bc3ae9c02f0c0f1acdcaece15c26
BroadCast247 <akkcheung@hotmail.com>
2024-01-29 20:24:07 -0500

    Pause Menu Creation
    Created Pause Menu Template.
    - Scene Transition
    - Pause and Resume
    - GameObject.SetActive() Toggles

---
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//GameObject stored in GameOverScene for now, should be compatible with any Game Scene.
+//Menu GameObject would be inactive by default, needs external dereference to toggle it.
+public class PauseMenuController : MonoBehaviour
+{
+    private Button[] buttons = new Button[2];
+
+    void Start()
+    {
+        buttons = GetComponentsInChildren<Button>();
+        Debug.Log(buttons[0].name + " " + buttons[1].name);
+
+        buttons[0].onClick.AddListener(Resume);
+        //Figure out Scene Index.
+        //buttons[1].onClick.AddListener(ChangeScene(sceneIndex));
+    }
+
+    //I just found out you can do this.
+    private void OnEnable()
+    {
+        Time.timeScale = 0;
+        Debug.Log(Time.timeScale);
+    }
+
+    //GameManager task?
+    private void Resume()
+    {
+        Time.timeScale = 1;
+        Debug.Log(Time.timeScale);
+        gameObject.SetActive(false);
+    }
+
+    private void ChangeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}

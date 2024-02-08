339af39de2088ea536ace0a6abe0d551a3a9d3e0
Jeroldine <148226060+Jeroldine@users.noreply.github.com>
2024-02-08 18:43:35 -0500

    Jerome - Refactor Enemies (#83)
    Summary:
    Look at EnemyRefactorTestScene. Made an EnemyBase class. TankRefactored,
    SuicideBomberRefactored, MeleeChaseRefactored, and RangedChaseRefactored
    scirpts inherit from EnemyBase clas. Located in RefactoredEnemies Folder
    in Scripts.
    
    Details:
    - created EnemyBase script
    - created TankRefactored script
    - added health bar to Tank
    - EnemyHealthBar script
    - modified EnemyManager script
    - EnemyHurtbox script
    - Blank scirpts for MeleeChaseRefactored, RangedChaseRefactored,
    SuicideBomberRefactored
    
    Bug Fixes:
    
    Deleted:

--- Assets/Scripts/Health/EnemyManager.cs
+++ Assets/Scripts/Health/EnemyManager.cs
@@ -2,12 +2,48 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    [SerializeField]
-    protected HealthValue enemyHealth;
+    [SerializeField] private float maxHealth = 100;
+    [SerializeField] private DropData itemDrops;
+
+    private EnemyHealthBar healthBar;
+    private float currentHealth;
+    public float health => currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyHealth.value = 100;
+        currentHealth = maxHealth;
+        healthBar = GetComponentInChildren<EnemyHealthBar>();
+        if (!healthBar) Debug.Log("No Health Bar reference");
+        if (!itemDrops) Debug.Log("No Drop Data given for enemy");
     }
+
+    public void LoseHealth(float healthChange)
+    {
+        currentHealth -= healthChange;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        healthBar.UpdateHealthBar(currentHealth, maxHealth);
+
+        if (currentHealth <= 0)
+            EnemyDeath();
+    }
+
+    private void EnemyDeath()
+    {
+        DropItem();
+        DeathEffect();
+        Destroy(gameObject);
+    }
+
+    private void DropItem()
+    {
+        Debug.Log("Item Drop not implemented");
+        //itemDrops.DropAt(transform);
+    }
+
+    private void DeathEffect()
+    {
+        Debug.Log("Death Effect not implemented");
+    }
+
 }

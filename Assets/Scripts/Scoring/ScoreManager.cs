193fa4a24e2949fcea17075c2bca1f84bd30c20b
TYitsJason <tyjason.yang@gmail.com>
2024-02-10 15:41:31 -0500

    Added Combo System
    Added combo system for the game, including a coroutine, combo thresholds, and a function to be called each time an enemy is killed, which will handle the combo logic and add score according to multiplier

--- Assets/Scripts/Scoring/ScoreManager.cs
+++ Assets/Scripts/Scoring/ScoreManager.cs
@@ -9,6 +9,19 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
 
     private int score;
+    public int baseEnemyKillScore = 50;
+    public int earlyWaveBonus = 50;
+    public int remainingHealthBonus = 100;
+    
+    // Combo system parameters
+    public int multiplier;
+    public float comboDuration = 2.0f;
+    public int comboThresholdX = 5; // Threshold for 2x multiplier
+    public int comboThresholdY = 10; // Threshold for 4x multiplier
+    public int comboThresholdZ = 20; // Threshold for 8x multiplier
+    private Coroutine comboTimerCoroutine; // Reference to the active combo timer coroutine
+    public int currentComboCount = 0;
+
 
     private void Awake()
     {
@@ -28,13 +41,72 @@ public class ScoreManager : MonoBehaviour
     {
         return score;
     }
-    void Start()
+
+    // TODO: Add a function to increase score that don't use the multiplier for events like early wave spawn bonus
+
+
+
+
+    // TODO: Add a function that gives bonus score at the end of level based on player's current health
+
+
+    
+    
+    // OR ALTERNATIVELY: Add a generic score increase function that will be called manually
+
+    
+    // Combo system
+
+    // The RegisterKill function should be called everytime an enemy is killed. We could also update this function to
+    // subscribe to an event listener instead if the enemy kills work on an event system
+    public void RegisterKill()
     {
-        
+        currentComboCount++;
+        if (comboTimerCoroutine != null)
+        {
+            StopCoroutine(comboTimerCoroutine);
+        }
+        comboTimerCoroutine = StartCoroutine(ComboTimer());
+        UpdateMultiplier();
+
+        score += baseEnemyKillScore * multiplier;
     }
 
-    void Update()
+    // This is the coroutine for the combo timer
+    private IEnumerator ComboTimer()
     {
-        
+        yield return new WaitForSeconds(comboDuration);
+        ResetCombo();
+    }
+
+    // This function will reset the combo to 0
+    private void ResetCombo()
+    {
+        currentComboCount = 0;
+        multiplier = 1;
+        comboTimerCoroutine = null;
+    }
+
+    // This function is called every time a kill happens within a combo, it will check the current combo count and update
+    // the multiplier accordingly
+    private void UpdateMultiplier()
+    {
+        if (currentComboCount >= comboThresholdZ)
+        {
+            multiplier = 8;
+        }
+        else if (currentComboCount >= comboThresholdY)
+        {
+            multiplier = 4;
+        }
+        else if (currentComboCount >= comboThresholdX)
+        {
+            multiplier = 2;
+        }
+        else
+        {
+            multiplier = 1;
+        }
     }
 }
+

db3b75e338280919ef8d7b7cfdcfc25cafdfd7bb
MChesbro97 <mitchell.chesbro@hotmail.com>
2024-02-14 16:23:31 -0500

    Laser and Shotgun
    -Added laser
    -Added laser functionality
    -Fixed shotgun bug where bullets weren't spawning
    -Added punching bag to test weapons

--- Assets/Scripts/MainAttackScript/ShootManager.cs
+++ Assets/Scripts/MainAttackScript/ShootManager.cs
@@ -10,17 +10,30 @@ public class ShootManager : MonoBehaviour
     [SerializeField] private Transform m_shootPoint;
     [SerializeField] private LaserShot m_laserShot;
 
-    private InventoryManager invM;
+    private SingleShot singleShot;
+    private SpreadShot spreadShot;
+    private BulletSpawner m_bulletSpawner;
+    private InventoryManager m_inventory;
 
+    // Live variables
+    private float m_lastFire1;
+    private float m_lastFire2;
+
+    public void Equip(Weapon weapon)
+    {
+        if (weapon == null) return;
+        if (weapon is Arm1) m_primary = weapon as Arm1;
+        if (weapon is Arm2) m_secondary = weapon as Arm2;
+    }
     void Start()
     {
         //need to null check A1 somehow
-        invM = FindObjectOfType<InventoryManager>();
-        if (!invM) Debug.Log("Shoot Manager can't find the Inventory Manager");
+        m_inventory = FindObjectOfType<InventoryManager>();
+        if (m_inventory) Debug.Log("Shoot Manager can't find the Inventory Manager");
 
         m_bulletSpawner = new BulletSpawner(gameObject, m_shootPoint);
-        singleShot = new SingleShot(gameObject, m_bulletSpawner);
-        spreadShot = new SpreadShot(gameObject, m_bulletSpawner);
+        singleShot = new SingleShot();
+        spreadShot = new SpreadShot();
         
 
         m_lastFire1 = 100.0f;
@@ -34,21 +47,35 @@ public class ShootManager : MonoBehaviour
     //SniperRifle
     public void LeftClickFire()
     {
+        ShotData shotData = m_primary.shotData;
+
+        // Get the modifiers
+        float attackMult = 1.0f;
+        float attackAdd = 0.0f;
+        float speedMult = 1.0f;
+        float speedAdd = 0.0f;
+        if (m_inventory != null)
+        {
+            attackMult = m_inventory.getAttackDamageMultiply();
+            attackAdd = m_inventory.getAttackDamageAdd();
+            speedMult = m_inventory.getAttackSpeedMultiply();
+            speedAdd = m_inventory.getAttackSpeedAdd();
+        }
         //Debug.Log("Left Click is Being Held");
-        
-        if (a1CurEquip is DefaultPistol)
+
+        if (m_primary is DefaultPistol)
         {
             Debug.Log("Pistol is Equipped");
             //pass the cur equip to the Single Shot script
         }
 
-        if (a1CurEquip is SMG)
+        if (m_primary is SMG)
         {
             Debug.Log("SMG is Equipped");
             //pass the cur equip to the Single Shot script
         }
 
-        if (a1CurEquip is SniperRifle)
+        if (m_primary is SniperRifle)
         {
             Debug.Log("Sniper is Equipped");
             //pass the cur equip to the Single Shot script
@@ -58,14 +85,14 @@ public class ShootManager : MonoBehaviour
         // Fire Rate Check
         float fireRate = (shotData.fireRate / speedMult) - speedAdd;
         fireRate = Mathf.Clamp(fireRate, 0.05f, 3.0f);
-        if (lastFire < fireRate)  return;
+        if (m_lastFire1 < fireRate)  return;
         
         // Modify Damage
-        float damage = (weapon.flatDamage * attackMult) +attackAdd;
+        float damage = (m_primary.flatDamage * attackMult) +attackAdd;
 
 
         // Special case for laser weapon
-        if (weapon is Laser)
+        if (m_secondary is Laser)
         {
             m_laserShot.Shoot(damage);
             return;
@@ -76,7 +103,7 @@ public class ShootManager : MonoBehaviour
         float spreadAngle = shotData.spreadAngle;
         if (spreadAngle > 5.0f) shot = spreadShot;
 
-        if (a1CurEquip is Shotgun)
+        if (m_primary is Shotgun)
         {
             Debug.Log("Shotgun is Equipped");
             //pass the cur equip to the Spread Shot script
@@ -90,6 +117,6 @@ public class ShootManager : MonoBehaviour
 
     void UpdateEquippedA1(Arm1 incoming)
     {
-        a1CurEquip = incoming;
+        m_primary = incoming;
     }
 }
